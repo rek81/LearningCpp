346b8c0cf7b0db3b0d2dba303f92204355772ef1
Rebecca Kowalski <rek81@scarletmail.rutgers.edu>
2020-06-18 22:19:32 -0400

    Update 06/18/2020

--- BinHistoFunc_New.cpp
+++ BinHistoFunc_New.cpp
@@ -9,55 +9,51 @@
 #include "TFile.h"
 #include "TH1F.h"
 #include "TH2F.h"
-using namespace std;
-//class base {
-//public:
-//  virtual void func()
-
-//}
-//  class BinHistoFunc_New : public base {
 
 class BinHistoFunc_New {
 public:
   TFile *F;
-  TH2F *hist2; 
-  TObject *hist = new TObject;
- //  string type;
+  //  void *hist;
+  //  TH1F *hist;
+  TH1F *hist1;
+  TH2F *hist2;
+  TH3F *hist3;
+  //  string type;
   BinHistoFunc_New(std::string filename, std::string histname){
     F = TFile::Open(filename.c_str());
     //    hist = (TH1F *)F->Get(histname.c_str());
-    hist2 = (TH2F *)F->Get(histname.c_str());
+    //    hist2 = (TH2F *)F->Get(histname.c_str());
+    if (dynamic_cast<TH1F*> ((TH1F *)F->Get(histname.c_str()))) {TH1F* hist = (TH1F *)F->Get(histname.c_str())}
+    else if (dynamic_cast<TH2F*> ((TH2F *)F->Get(histname.c_str()))) {TH2F* hist = (TH2F *)F->Get(histname.c_str())}
+      //string histstring = histname.c_str();
     //    type = histtype.c_str();   
   };
 
-  float GetBinContent(void){ 
-        float val;
-	//	base *hist 
-
-	//        if (dynamic_cast<TH1F *>(&hist)){
-	if (TH1F *hnew = dynamic_cast<TH1*> (hist = (TH1F *)F->Get(histname.c_str()))){
-	//	if (TClass::InheritsFrom(TH1)){
-	    //	    F->Get(histname.c_str();
-	  int binsx = hnew->GetNbinsX();
-	  std::cout << "hi" << std::endl;
-	  std::cout << binsx << std::endl;
-	  for(int b = 1; b < binsx; b++){
-	    val = hnew->GetBinContent(b);
-	    std::cout << "val is " << val << std::endl;
-	    std::cout << "bin is " << b << std::endl;
-	          };
-	  } else if (dynamic_cast<TH2F *> (hist = (TH2F *)F->Get(histname.c_str())){
-	  int binsx = hist2->GetNbinsX();
-	  int binsy = hist2->GetNbinsY();
-	  for(int bx = 1; bx < binsx; bx++){
-	    for(int by = 1; by < binsy; by++){
-	      float val = hist2->GetBinContent(bx, by);
-	      std::cout << "val is " << val << std::endl;
-	    };
-	  };
-	}
-	else {}
-        return val; 
+  //  float GetBinContent(void){ 
+  float GetBinContent(){ 
+    float val;
+    //    if (dynamic_cast<TH1F*> (hist = F->Get(histstring))){
+    //    if {
+      int binsx = hist->GetNbinsX();
+      std::cout << "hi" << std::endl;
+      std::cout << binsx << std::endl;
+      for(int b = 1; b < binsx; b++){
+	val = hist->GetBinContent(b);
+	std::cout << "val is " << val << std::endl;
+	std::cout << "bin is " << b << std::endl;
+      };
+    } else if (type=="2D"){
+      int binsx = hist2->GetNbinsX();
+      int binsy = hist2->GetNbinsY();
+      for(int bx = 1; bx < binsx; bx++){
+	for(int by = 1; by < binsy; by++){
+	  float val = hist2->GetBinContent(bx, by);
+	  std::cout << "val is " << val << std::endl;
+	};
+      };
+    }
+    else {}
+    return val; 
   };
   float GetBinContentByAxis(float xmin = 0 , float xmax = 0 , float ymin = 0, float ymax = 0) {
     float val;
@@ -98,7 +94,6 @@ public:
 };
 
 
-//BinHistoFunc_New obj =  BinHistoFunc_New("1D", "/home/rek81/userArea/CMSSW_10_2_2/src/trigger_turn_on/Final_for_Analysis_Note/SingleMuon/HTcut900/outputs/triggerRatePlots_RunE.root", "HTTrigNum");
 BinHistoFunc_New obj =  BinHistoFunc_New("/home/rek81/userArea/CMSSW_10_2_2/src/trigger_turn_on/Final_for_Analysis_Note/SingleMuon/HTcut900/outputs/triggerRatePlots_RunE.root", "HTTrigNum");
 float a = obj.GetBinContent();
 //float a = obj.GetBinContentByAxis(1000, 1500);
