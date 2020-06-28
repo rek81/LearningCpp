39b32b2b029ebe65651a1a062350a7cf8ec6880c
Rebecca Kowalski <rek81@scarletmail.rutgers.edu>
2020-06-28 16:12:41 -0400

    update 06/28/2020

--- BinHistoFunc_New.cpp
+++ BinHistoFunc_New.cpp
@@ -13,36 +13,51 @@
 class BinHistoFunc_New {
 public:
   TFile *F;
-  //  void *hist;
-  //  TH1F *hist;
   TH1F *hist1;
   TH2F *hist2;
   TH3F *hist3;
   //  string type;
   BinHistoFunc_New(std::string filename, std::string histname){
+    std::cout << "in constructor" << std::endl;
     F = TFile::Open(filename.c_str());
-    //    hist = (TH1F *)F->Get(histname.c_str());
-    //    hist2 = (TH2F *)F->Get(histname.c_str());
-    if (dynamic_cast<TH1F*> ((TH1F *)F->Get(histname.c_str()))) {TH1F* hist = (TH1F *)F->Get(histname.c_str())}
-    else if (dynamic_cast<TH2F*> ((TH2F *)F->Get(histname.c_str()))) {TH2F* hist = (TH2F *)F->Get(histname.c_str())}
-      //string histstring = histname.c_str();
-    //    type = histtype.c_str();   
+    std::cout << "file in constructor is " << F << std::endl;
+
+    TH1F* hist1 = dynamic_cast<TH1F*> (F->Get(histname.c_str()));
+    TH2F* hist2 = dynamic_cast<TH2F*> (F->Get(histname.c_str()));
+    TH3F* hist3 = dynamic_cast<TH3F*> (F->Get(histname.c_str()));
+
+    std::cout << "histo1 in constructor is " << hist1 << std::endl;
+    std::cout << "histo2 in constructor is " << hist2 << std::endl;
+    std::cout << "histo3 in constructor is " << hist3 << std::endl;
   };
 
-  //  float GetBinContent(void){ 
-  float GetBinContent(){ 
+  float GetBinContent(void){ 
+    TH1F* hist1;
+    TH1F* hist2;
+    TH1F* hist3;
+    
+    std::cout << "hist1 is " << hist1 << std::endl;
+    std::cout << "hist2 is " << hist2 << std::endl;
+    std::cout << "hist3 is " << hist3 << std::endl;
+    //    std::cout << "file in def is " << F << std::endl;
+    //    std::cout << "histo in def is " << hist1 << std::endl;
+  
+    
     float val;
-    //    if (dynamic_cast<TH1F*> (hist = F->Get(histstring))){
-    //    if {
-      int binsx = hist->GetNbinsX();
-      std::cout << "hi" << std::endl;
+    if (hist1 != 0) {
+      std::cout << "in if of func" << std::endl;
+      int binsx = hist1->GetNbinsX();
       std::cout << binsx << std::endl;
+      std::cout << "bot bins" << std::endl;
       for(int b = 1; b < binsx; b++){
-	val = hist->GetBinContent(b);
+	std::cout << "in for loop of bincontent" << std::endl;
+	val = hist1->GetBinContent(b);
 	std::cout << "val is " << val << std::endl;
 	std::cout << "bin is " << b << std::endl;
       };
-    } else if (type=="2D"){
+    }
+  
+    else if (hist2 != 0){
       int binsx = hist2->GetNbinsX();
       int binsy = hist2->GetNbinsY();
       for(int bx = 1; bx < binsx; bx++){
@@ -52,23 +67,24 @@ public:
 	};
       };
     }
+  
     else {}
     return val; 
   };
   float GetBinContentByAxis(float xmin = 0 , float xmax = 0 , float ymin = 0, float ymax = 0) {
     float val;
-    if (type=="1D"){ 
+    if (hist1){ 
       float Xmin = xmin;
       float Xmax = xmax;
-      TAxis *xaxis = hist->GetXaxis();
+      TAxis *xaxis = hist1->GetXaxis();
       int bxmin = xaxis->FindBin(Xmin);
       int bxmax = xaxis->FindBin(Xmax);
       for(int bx = bxmin; bx < bxmax; bx++){
-	val = hist->GetBinContent(bx);
+	val = hist1->GetBinContent(bx);
 	std::cout << "val is " << val << std::endl;
       }
     }
-    else if (type=="2D"){
+    else if (hist2){
       std::cout << "I am in 2D if statment" << std::endl;
       float Xmin = xmin;
       float Xmax = xmax;
