325df99db5cde82b2c328856804954230ef5acb8
Rebecca Kowalski <rek81@scarletmail.rutgers.edu>
2020-06-28 20:32:24 -0400

    new

--- BinHistoFunc_New.cpp
+++ BinHistoFunc_New.cpp
@@ -16,15 +16,14 @@ public:
   TH1F *hist1;
   TH2F *hist2;
   TH3F *hist3;
-  //  string type;
   BinHistoFunc_New(std::string filename, std::string histname){
     std::cout << "in constructor" << std::endl;
     F = TFile::Open(filename.c_str());
     std::cout << "file in constructor is " << F << std::endl;
 
-    TH1F* hist1 = dynamic_cast<TH1F*> (F->Get(histname.c_str()));
-    TH2F* hist2 = dynamic_cast<TH2F*> (F->Get(histname.c_str()));
-    TH3F* hist3 = dynamic_cast<TH3F*> (F->Get(histname.c_str()));
+    hist1 = dynamic_cast<TH1F*> (F->Get(histname.c_str()));
+    hist2 = dynamic_cast<TH2F*> (F->Get(histname.c_str()));
+    hist3 = dynamic_cast<TH3F*> (F->Get(histname.c_str()));
 
     std::cout << "histo1 in constructor is " << hist1 << std::endl;
     std::cout << "histo2 in constructor is " << hist2 << std::endl;
@@ -32,25 +31,16 @@ public:
   };
 
   float GetBinContent(void){ 
-    TH1F* hist1;
-    TH1F* hist2;
-    TH1F* hist3;
-    
     std::cout << "hist1 is " << hist1 << std::endl;
     std::cout << "hist2 is " << hist2 << std::endl;
     std::cout << "hist3 is " << hist3 << std::endl;
-    //    std::cout << "file in def is " << F << std::endl;
-    //    std::cout << "histo in def is " << hist1 << std::endl;
-  
     
     float val;
     if (hist1 != 0) {
       std::cout << "in if of func" << std::endl;
       int binsx = hist1->GetNbinsX();
       std::cout << binsx << std::endl;
-      std::cout << "bot bins" << std::endl;
       for(int b = 1; b < binsx; b++){
-	std::cout << "in for loop of bincontent" << std::endl;
 	val = hist1->GetBinContent(b);
 	std::cout << "val is " << val << std::endl;
 	std::cout << "bin is " << b << std::endl;
